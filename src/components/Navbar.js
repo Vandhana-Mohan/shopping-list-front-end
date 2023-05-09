35191db1f1bcaf177e45193389152d92fa4e288c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-09 02:50:55 -0400

    updated header, navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="bg-pink-200 py-4 flex justify-around">
-      <h1 className="text-4xl font-bold mb-6">
+      {/* <h1 className="text-4xl font-bold mb-6">
         <Link to="/">FreshPick </Link> Welcomes You
-      </h1>
+      </h1> */}
       <ul className="flex justify-around">
         <li className="cursor-pointer hover:text-gray-300 mr-4">
           {" "}
@@ -26,10 +26,10 @@ function Navbar() {
           </Link>
         </li>
 
-        <li className="cursor-pointer hover:text-gray-300">
+        <li className="cursor-pointer hover:text-gray-300 bg-pink-400 text-white py-2 px-4 rounded">
           Number of items in cart{" "}
         </li>
-        <li className="cursor-pointer hover:text-gray-300">
+        <li className="cursor-pointer hover:text-gray-300 bg-pink-400 text-white py-2 px-4 rounded">
           {" "}
           Total price of cart{" "}
         </li>
