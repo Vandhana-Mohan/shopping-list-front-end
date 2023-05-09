b125c64307127d9516b2a115ebe9ca7e5e980e7c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-09 00:53:17 -0400

    set up env

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,7 +1,8 @@
+import Cart_Logo from "../assets/Cart_Logo.png";
 function Header() {
   return (
     <div>
-      <img src="" alt="Shop_Logo"></img>
+      <img src={Cart_Logo} alt="Shop_Logo"></img>
       <h1> Welcome to shopping </h1>
     </div>
   );
