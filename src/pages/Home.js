d528325e8b3685813c94278e8c4573009c6a8dad
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-09 13:01:48 -0400

    have a dummy user in header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 function Home() {
   return (
     <div className="flex flex-col items-center m-6 p-6 justify-center ">
-      <h1 className="text-4xl font-bold mb-6">FreshPick Welcomes You</h1>
+      <h1 className="text-4xl font-bold mb-6">
+        Good Morning Ms. M, FreshPick welcomes you
+      </h1>
       <p className="text-lg text-gray-500">
         Make your grocery shopping hassle-free and never forget an item again !!
       </p>
