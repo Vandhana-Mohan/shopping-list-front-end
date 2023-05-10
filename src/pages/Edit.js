f3182159c5d79798820fcecb582c511e999f70a8
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-10 00:12:49 -0400

    add four0four page

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,12 @@
-import BudgetEditForm from "../Components/BudgetEditForm";
+import GroceryEditForm from "../components/GroceryEditForm";
 
 function Edit() {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-6">
-      <h2 className="text-xl font-bold mb-4">Add New Transaction</h2>
-      <BudgetEditForm />
+    <div className="shadow-lg rounded-lg items-center p-6 m-6 justify-center bg-pink-50">
+      <h1 className="text-4xl font-bold mb-6">
+        Edit items from your cart ...{" "}
+      </h1>
+      <GroceryEditForm />
     </div>
   );
 }
--- src/pages/FourOFour.js
+++ src/pages/FourOFour.js
@@ -1,7 +1,9 @@
 function FourOFour() {
   return (
-    <div className="flex justify-center items-center py-24">
-      <h1 className="text-4xl font-bold text-gray-700">Sorry, no page found</h1>
+    <div className="flex justify-center shadow-lg rounded-lg items-center bg-pink-50 p-6 m-6 py-24">
+      <h1 className="text-4xl font-bold text-gray-700">
+        Sorry, page not found ! ! !
+      </h1>
     </div>
   );
 }
