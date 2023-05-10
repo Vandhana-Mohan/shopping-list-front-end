3d38115cdd89f2c67e70fa91e382dd2e998b2422
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-10 03:51:31 -0400

    display all groceries

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,8 +2,8 @@ import Groceries from "../components/Groceries";
 
 function Index() {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold mb-4">View Items in Cart</h1>
+    <div className="flex flex-col items-center m-6 p-6 justify-center bg-pink-50">
+      <h1 className="text-4xl font-bold mb-6">View All Items in Cart</h1>
       <Groceries />
     </div>
   );
