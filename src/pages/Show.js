a22b9d36431cac358479cbe34727336e03fe669e
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-11 16:50:54 -0400

    update css for show one item

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -2,9 +2,10 @@ import GroceryDetails from "../components/GroceryDetails";
 
 function Show() {
   return (
-    <div className="flex flex-col p-6 m-6 rounded-lg items-center justify-center shadow-green-500/50 border shadow-md">
-      <h1 className="text-4xl font-bold mb-6">View Details . . .</h1>
+    <div className="p-6 m-6 rounded-lg items-center justify-center shadow-green-500/50 border shadow-md" style={{ paddingBottom: "10rem" }}>
+      <h1 className="text-4xl text-center font-bold mb-6">View Details . . .</h1>
       <GroceryDetails />
+      {/* </div> */}
     </div>
   );
 }
