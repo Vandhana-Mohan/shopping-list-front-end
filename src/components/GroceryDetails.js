c034e27a9e7af3cf02e8431aeee13f5fdd4a99c1
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-11 11:18:19 -0400

    merged with main

--- src/components/GroceryDetails.js
+++ src/components/GroceryDetails.js
@@ -101,6 +101,7 @@ function GroceryDetails() {
         </div>
 
         {/* <Reviews /> */}
+        
         <div className="px-4 py-3 sm:px-6">
           <div className="flex justify-center">
             <button className="mr-6 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded shadow-sm text-sm font-medium text-white bg-pink-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
