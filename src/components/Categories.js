b641f7d1400f1268cfb68333a99fd19c2ff1ee2b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-13 19:35:39 -0400

    connect categories to groceries

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,70 +1,89 @@
+import { useState } from "react";
+import Groceries from "./Groceries";
+
 function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
       <div className="flex flex-col items-center">
         <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <p className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
+          <button
+            onClick={() => handleCategoryClick("dairy")}
+            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
+          >
             Dairy
-          </p>
+          </button>
         </div>
       </div>
 
       <div className="flex flex-col items-center">
         <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <p className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
+          <button
+            onClick={() => handleCategoryClick("vegetables")}
+            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
+          >
             Vegetables
-          </p>
+          </button>
         </div>
       </div>
       <div className="flex flex-col items-center">
         <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <p className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
+          <button
+            onClick={() => handleCategoryClick("fruits")}
+            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
+          >
             Fruits
-          </p>
+          </button>
         </div>
       </div>
       <div className="flex flex-col items-center">
         <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <p className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
+          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
             Bakery
-          </p>
+          </button>
         </div>
       </div>
       <div className="flex flex-col items-center">
         <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <p className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
+          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
             Canned Goods
-          </p>
+          </button>
         </div>
       </div>
       <div className="flex flex-col items-center">
         <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <p className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
+          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
             Meat
-          </p>
+          </button>
         </div>
       </div>
       <div className="flex flex-col items-center">
         <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <p className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
+          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
             Beverages
-          </p>
+          </button>
         </div>
       </div>
       <div className="flex flex-col items-center">
         <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <p className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide ">
+          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide ">
             Snacks
-          </p>
+          </button>
         </div>
       </div>
       <div className="flex flex-col items-center">
         <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <p className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
+          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
             Frozen Foods
-          </p>
+          </button>
         </div>
       </div>
+      <Groceries category={selectedCategory} />
     </div>
   );
 }
--- src/components/Groceries.js
+++ src/components/Groceries.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-
 import Grocery from "./Grocery";
 
-function Groceries() {
+function Groceries({ category }) {
   const [grocery, setGrocery] = useState([]);
+  const [filteredGrocery, setFilteredGrocery] = useState([]);
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/groceries`)
@@ -16,9 +16,23 @@ function Groceries() {
       });
   }, []);
 
+  useEffect(() => {
+    if (category) {
+      const filteredItems = grocery.filter(
+        (item) => item.category.toLowerCase() === category.toLowerCase()
+      );
+      setFilteredGrocery(filteredItems);
+    } else {
+      setFilteredGrocery(grocery);
+    }
+  }, [category, grocery]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded-lg shadow-green-500/50 border shadow-md">
       {grocery.map((item) => (
+        <Grocery key={item.id} grocery={item} />
+      ))}
+      {filteredGrocery.map((item) => (
         <Grocery key={item.id} grocery={item} />
       ))}
     </div>
