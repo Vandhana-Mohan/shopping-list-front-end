0ceb903653be1729c25874e231411e95e4555c04
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-15 10:37:33 -0400

    edit review css

--- src/components/Review.js
+++ src/components/Review.js
@@ -9,7 +9,13 @@ function Review({ review, handleDelete, handleSubmit }) {
   };
   return (
     <div className="Review">
-      <button onClick={toggleView}>Edit this review</button>
+      <button
+        type="button"
+        className="bg-green-300 hover:bg-green-100 mt-4 text-xl font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline rounded-lg shadow-green-500/50 border shadow-md"
+        onClick={toggleView}
+      >
+        Edit this review
+      </button>
       {viewEditForm ? (
         <ReviewForm
           reviewDetails={review}
@@ -17,15 +23,25 @@ function Review({ review, handleDelete, handleSubmit }) {
           toggleView={toggleView}
         />
       ) : (
-        <div>
-          <h4>
-            {review.title} <span>{review.rating}</span>
+        <div
+          className="flex flex-col items-center m-6 p-6 justify-center rounded-lg shadow-green-500/50 border shadow-md"
+          style={{ paddingBottom: "10rem" }}
+        >
+          <h4 className="text-lg font-bold p-6 mb-6">Title : {review.title}</h4>
+          <h4 className="text-lg font-bold p-6 mb-6">
+            Rating : {review.rating}
           </h4>
-          <h5>{review.reviewer}</h5>
-          <p>{review.content}</p>
+          <h5 className="text-lg font-bold mb-6">Reviewer : {review.reviewer}</h5>
+          <p className="text-lg font-bold mb-6">Description: {review.content}</p>
         </div>
       )}
-      <button onClick={() => handleDelete(review.id)}>Delete</button>
+      <button
+        type="button"
+        className="bg-green-300 hover:bg-green-100 mt-4 text-xl font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline rounded-lg shadow-green-500/50 border shadow-md"
+        onClick={() => handleDelete(review.id)}
+      >
+        Delete
+      </button>
     </div>
   );
 }
--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 function ReviewForm(props) {
   let { id } = useParams();
+  const navigate = useNavigate();
   const { reviewDetails } = props;
 
   const [review, setReview] = useState({
@@ -39,50 +40,85 @@ function ReviewForm(props) {
     });
   };
   return (
-    <div className="Edit">
+    <div className="p-4" style={{ paddingBottom: "6rem" }}>
       {props.children}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="reviewer">Name:</label>
-        <input
-          id="reviewer"
-          value={review.reviewer}
-          type="text"
-          onChange={handleTextChange}
-          placeholder="Your name"
-          required
-        />
-        <label htmlFor="title">Title:</label>
-        <input
-          id="title"
-          type="text"
-          required
-          value={review.title}
-          onChange={handleTextChange}
-        />
-        <label htmlFor="rating">Rating:</label>
-        <input
-          id="rating"
-          type="number"
-          name="rating"
-          min="0"
-          max="5"
-          step="1"
-          value={review.rating}
-          onChange={handleTextChange}
-        />
-        <label htmlFor="content">Review:</label>
-        <textarea
-          id="content"
-          type="text"
-          name="content"
-          value={review.content}
-          placeholder="What do you think..."
-          onChange={handleTextChange}
-        />
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="flex flex-col w-3/4">
+          <label htmlFor="reviewer" className="mb-1 font-medium text-gray-700">
+            Name:
+          </label>
+          <input
+            id="reviewer"
+            value={review.reviewer}
+            type="text"
+            onChange={handleTextChange}
+            placeholder="Your name"
+            required
+            className="border border-gray-300 rounded-md px-3 py-2 relative bg-white shadow outline-none focus:outline-none focus:ring lg:w-3/4"
+          />
+        </div>
 
-        <br />
+        <div className="flex flex-col w-3/4">
+          <label htmlFor="title" className="mb-1 font-medium text-gray-700">
+            Title:
+          </label>
+          <input
+            id="title"
+            type="text"
+            required
+            value={review.title}
+            onChange={handleTextChange}
+            className="border border-gray-300 rounded-md px-3 py-2 relative bg-white shadow outline-none focus:outline-none focus:ring lg:w-3/4"
+          />
+        </div>
 
-        <input type="submit" />
+        <div className="flex flex-col w-3/4">
+          <label htmlFor="rating" className="mb-1 font-medium text-gray-700">
+            Rating:
+          </label>
+          <input
+            id="rating"
+            type="number"
+            name="rating"
+            min="0"
+            max="5"
+            step="1"
+            value={review.rating}
+            onChange={handleTextChange}
+            className="border border-gray-300 rounded-md px-3 py-2 relative bg-white shadow outline-none focus:outline-none focus:ring lg:w-3/4"
+          />
+        </div>
+
+        <div className="flex flex-col w-full">
+          <label htmlFor="content" className="mb-1 font-medium text-gray-700">
+            Review:
+          </label>
+          <textarea
+            id="content"
+            type="text"
+            name="content"
+            value={review.content}
+            placeholder="What do you think..."
+            onChange={handleTextChange}
+            className="border border-gray-300 rounded-md px-3 py-2 relative bg-white shadow outline-none focus:outline-none focus:ring lg:w-3/4"
+          />
+        </div>
+        <div className="flex justify-center space-x-4">
+          <button
+            type="submit"
+            onSubmit={handleSubmit}
+            className="bg-green-300 hover:bg-green-100 mt-4 text-xl font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline rounded-lg shadow-green-500/50 border shadow-md"
+          >
+            Save
+          </button>
+          <button
+            type="button"
+            onClick={() => navigate(-1)}
+            className="bg-green-300 hover:bg-green-100 mt-4 text-xl font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline rounded-lg shadow-green-500/50 border shadow-md"
+          >
+            Cancel
+          </button>
+        </div>
       </form>
     </div>
   );
